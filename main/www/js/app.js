5929219621cffeef53c6920b7277dc0a30591bd6
IeonChris <christophermaynard1989@gmail.com>
2016-05-25 14:18:39 -0400

    Moving the tab to the bottom

--- main/www/js/app.js
+++ main/www/js/app.js
@@ -23,8 +23,11 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $ionicConfigProvider, $urlRouterProvider) {
 
+
+
+$ionicConfigProvider.tabs.position('bottom'); //bottom
   // Ionic uses AngularUI Router which uses the concept of states
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
